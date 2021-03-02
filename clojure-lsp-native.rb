45364500afe173569090bae00a758e78269e2edb
Eric Dallo <ericdallo06@hotmail.com>
2021-03-02 09:33:09 -0300

    Bump clojure-lsp

--- clojure-lsp-native.rb
+++ clojure-lsp-native.rb
@@ -2,9 +2,9 @@ class ClojureLspNative < Formula
 
   desc "Language Server (LSP) for Clojure"
   homepage "https://github.com/clojure-lsp/clojure-lsp"
-  version "2021.02.24-14.23.08"
-  url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2021.02.24-14.23.08/clojure-lsp-native-macos-amd64.zip"
-  sha256 "7296d118381303a91a24b4789fe7e374ddfb331415f7ae235d7626b5d80a83ee"
+  version "2021.03.01-19.18.54"
+  url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2021.03.01-19.18.54/clojure-lsp-native-macos-amd64.zip"
+  sha256 "ae0519c98b8968c0b03f7431dd380ebbce03faa5fcb08f48aa3460f147be300c"
 
   bottle :unneeded
 
