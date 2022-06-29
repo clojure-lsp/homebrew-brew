784dfd33fa9762b4e59fbe30fa17750eedb51c77
Eric Dallo <ercdll1337@gmail.com>
2022-06-29 19:14:11 -0300

    Update clojure-lsp-native.rb

--- clojure-lsp-native.rb
+++ clojure-lsp-native.rb
@@ -6,7 +6,7 @@ class ClojureLspNative < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2022.06.29-19.32.13/clojure-lsp-native-macos-aarch64.zip"
-      sha256 ""
+      sha256 "efd098b007c156c3eebb458a04bcaa75c2b67de40eff6f3321b9c78d7fc0eb14"
     else
       url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2022.06.29-19.32.13/clojure-lsp-native-macos-amd64.zip"
       sha256 "36b51938b18986a3a75d57144baceaf8dc61d1693be553948cd51d616b5cc9b2"
