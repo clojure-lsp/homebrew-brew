ade48f1c885c6da0a5a7d9bb040840a1dcf17474
Eric Dallo <ericdallo06@hotmail.com>
2023-02-27 12:44:21 -0300

    Fix sha

--- clojure-lsp-native.rb
+++ clojure-lsp-native.rb
@@ -8,7 +8,7 @@ class ClojureLspNative < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2023.02.27-13.12.12/clojure-lsp-native-macos-aarch64.zip"
-      sha256 ""
+      sha256 "2df117c7fd73c009f1dc6613468cf8473f62fe40fd4b73d5327f4aa6c99e6de0"
     else
       url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2023.02.27-13.12.12/clojure-lsp-native-macos-amd64.zip"
       sha256 "63effc06ca3f9d7f914461c19e6a11e29a845aaf905bd7f023a69c456be3fa57"
@@ -19,7 +19,7 @@ class ClojureLspNative < Formula
       sha256 "7b2eabaf1f4e4c686aabc4f3a8ae800accf12880deff2dfd8838a83ce08186d6"
     else
       url "https://github.com/clojure-lsp/clojure-lsp/releases/download/2023.02.27-13.12.12/clojure-lsp-native-static-linux-amd64.zip"
-      sha256 ""
+      sha256 "c23a5c9029b3a548a6b8e66a0662103c13e44f220ad8e3f97abf0b7c53a994b1"
     end
   end
 
